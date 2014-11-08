d86a7885dec654e49bbef09656dab1470a7bbb9e
smurray47 <smurray.buzz@gmail.com>
2014-11-08 20:14:58 +0000

    Dadd:wq Dddsf B B B A A

---
+++ Makefile
@@ -0,0 +1,2 @@
+server: socket_setup.cpp
+	g++ -o server -ggdb3 -Wall -Werror -pedantic -std=gnu++98 socket_setup.cpp
--- socket_setup.cpp
+++ socket_setup.cpp
@@ -10,41 +10,72 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <netdb.h>
+#include <cerrno>
+#include <cstring>
+#include <unistd.h>
+
 
 int main(int argc, char ** argv) {
+ 
+  struct sockaddr_in client_addr;
 
-  const int port_number = 80;
-  int client_addr_size;
+  socklen_t client_addr_size;
+  client_addr_size = sizeof(client_addr); 
+
   int server_socket;
   int response_socket;
   int read_length;
-  
+  int status;
   
   char buffer [256];
 
-  struct sockaddr_in server_addr, client_addr;
+  struct addrinfo host_info;
+  struct addrinfo* host_info_list;
+ 
+
+  memset(&host_info, 0 , sizeof(host_info));
+ 
+
+  host_info.ai_family = AF_INET;
+  host_info.ai_socktype = SOCK_STREAM;
+
+
+  host_info.ai_flags = AI_PASSIVE;
+  status = getaddrinfo(NULL, "8080", &host_info, &host_info_list);
+  if(status < 0){
+    fprintf(stderr, "error getting host info");
+  }
+   
+
+ 
   server_socket = socket (AF_INET, SOCK_STREAM, 0);
   if (server_socket < 0){
     fprintf(stderr, "COULD NOT OPEN SOCKET");
   }
 
   
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port_number);
   
-  server_addr.sin_addr.s_addr = INADDR_ANY;
  
-  if (bind(server_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0){
-    fprintf(stderr,"BINDING FAILED");
+
+  int yes = 1;
+  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+  if (bind(server_socket, host_info_list->ai_addr, host_info_list->ai_addrlen) < 0){
+    fprintf(stderr,"BINDING FAILED: %d", errno);
+    std::cout<<std::strerror(errno)<<"\n";
   }
 
-  listen(server_socket, 5);
+  if(listen(server_socket, 5)< 0){
+    fprintf(stderr, "Listening Failed");
+  }
 
 
-  client_addr_size = sizeof(client_addr);
 
+ 
+  std::cout<<"waiting for connection\n";
   response_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
-
+  std::cout<<"Connection received\n";
   if(response_socket < 0){
     fprintf(stderr, "ERROR ACCEPTING");
   }
@@ -56,6 +87,12 @@ int main(int argc, char ** argv) {
   if(read_length < 0) {
     fprintf(stderr, "ERROR READING SOCKET");
   }
+
+  if(read_length == 0){
+    fprintf(stderr, "Host Lost");
+}
+
+  buffer[read_length] = '\0';
 
   std::cout<< "Here is the message: \n" << buffer; 
 
