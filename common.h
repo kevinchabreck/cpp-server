825a7b585476670697e1690ef38bdf45da676b37
smurray47 <smurray.buzz@gmail.com>
2014-12-02 16:55:53 +0000

    added msg bodies to be returned in error messages

--- common.h
+++ common.h
@@ -12,6 +12,7 @@ void send401(ConnObj* conn_state);
 void send403(ConnObj* conn_state);
 void send404(ConnObj* conn_state);
 void send411(ConnObj* conn_state);
+void sendHTML(ConnObj* conn_state, std::string status);
 void send500(ConnObj* conn_state);
 // helper methods
 std::string getTimestamp();
