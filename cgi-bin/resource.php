88f4047e5cfb4732f40168d6e749f15fdfe8d13f
Kevin Chabreck <kevin.chabreck@gmail.com>
2014-12-02 21:14:41 -0500

    sends arguments in body to specified resource

--- cgi-bin/resource.php
+++ cgi-bin/resource.php
@@ -1,7 +1,10 @@
 <?php
+	// echo "argv:\n";
+	// var_dump($argv);
 	echo "content:\n";
 	// reverses all of its inputs
 	for ($x = 1; $x < $argc; $x++) {
 		echo strrev($argv[$x]." ");
 	}
+	echo "\n";
 ?>
