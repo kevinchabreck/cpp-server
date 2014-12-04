2ae6da0bd6f01089b07fbb7d2a1b402ac01a67b2
Kevin Chabreck <kevin.chabreck@gmail.com>
2014-12-03 21:49:22 -0500

    even more cleanup

--- put.cpp
+++ put.cpp
@@ -18,19 +18,14 @@ void putResponse(Request* req, ConnObj* conn_state){
   std::string requested_obj = "www" + req->request_URI;
   std::string filename;
   std::string dir;
-  
   //Check if folder is authorized for PUTs
-  
   std::string path = req->request_URI;
   int loc = path.find_last_of("/");
   std::string rel_path = get_relpath(path.substr(0, loc + 1));
-  
   int allowed = conn_state->authorized(req->request_method, rel_path.substr(3, std::string::npos));
-  
   if(!allowed){  
     send403(conn_state);
   }
-
   else{
     //If function needs a continue response, send it
     if((req->headers).count("expect") == 1){
@@ -38,18 +33,16 @@ void putResponse(Request* req, ConnObj* conn_state){
         send100(conn_state);
       }
     }
-    
     //Check if file exists already
     struct stat st;
     int exists = stat(requested_obj.c_str(), &st);
     if(exists == 0){
       if(S_ISDIR(st.st_mode)){
-	if(mode==DEBUG){log("Tried to override a folder with file of the same name");}
-	send403(conn_state);
-	return;
+        if(mode==DEBUG){log("Tried to override a folder with file of the same name");}
+        send403(conn_state);
+        return;
       }
     }
-    
     //Open file to write into
     FILE* dest = fopen(requested_obj.c_str(), "w");
     if (dest == NULL){
@@ -57,7 +50,6 @@ void putResponse(Request* req, ConnObj* conn_state){
       send500(conn_state);
       return;
     }
-   
     //Figue out how long body will be
     long int n = 0;
     char buffer[8000];
@@ -65,15 +57,12 @@ void putResponse(Request* req, ConnObj* conn_state){
       send411(conn_state);
       return;
     }
-
     std::string size = req->headers["content-length"];
     std::cout << "Content length expected: "<< size << "\n";
-    
     char* ptr = NULL;
     long int total = strtol(size.c_str(), &ptr, 10);
     long int sum = 0;
     int to_read;
-
     //Read 8000 bytes at a time from body, then read remaining
     //Keep running sum so can read exactly whats there and not end
     //up blocking
@@ -89,9 +78,7 @@ void putResponse(Request* req, ConnObj* conn_state){
       bzero(buffer, 8000);
       sum = sum + n;
     }   
-    
     fclose(dest);
-    
     char mode[]= "0755";
     int p;
     p = strtol(mode, 0, 8);
@@ -103,8 +90,5 @@ void putResponse(Request* req, ConnObj* conn_state){
     else{
       send204(conn_state);
     }
-
   }
- 
 }
-
